f8c7d280506972977d7f08a627f36eaf91ac81c5
Luis F. Ayuso <luis.f.ayuso@gmail.com>
2024-05-15 13:01:57 +0200

    rename app

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Trench Crusade',
+      title: 'Liber Excommunicationum',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
