01ad7e4e9ccf6e7580cc79034140ef2ee70da964
Luis F. Ayuso <luis.f.ayuso@gmail.com>
2024-06-06 18:44:51 +0200

    Remove awful printing library and use just a pdf download.

--- lib/print.dart
+++ lib/print.dart
@@ -1,15 +1,20 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:printing/printing.dart';
 import 'package:provider/provider.dart';
 import 'package:tc_thing/model/model.dart';
 import 'package:tc_thing/model/warband.dart';
 
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:web/web.dart' as web;
 
-const sep = 120.0;
-
-pw.Document renderPdf(WarbandModel warband, Roster roster, Armory armory) {
+Future<pw.Document> renderPdf(
+  WarbandModel warband,
+  Roster roster,
+  Armory armory,
+) async {
   final doc = pw.Document();
 
   doc.addPage(pw.MultiPage(
@@ -92,113 +97,123 @@ pw.Container warriorBlock(WarriorModel warrior, Armory armory) {
         ]),
         pw.Table(children: [
           pw.TableRow(children: [
-            pw.Text("Cost"),
             pw.Text("Movement"),
             pw.Text("Ranged"),
             pw.Text("Melee"),
             pw.Text("Armour"),
+            pw.Text("Cost"),
           ]),
           pw.TableRow(children: [
-            pw.Text("${warrior.totalCost}"),
             pw.Text(warrior.type.movement),
             pw.Text("${warrior.type.ranged}"),
             pw.Text("${warrior.type.melee}"),
             pw.Text("${warrior.computeArmorValue(armory)}"),
+            pw.Text("${warrior.totalCost}"),
           ])
         ]),
 
         ...(ranged.isNotEmpty
             ? [
                 pw.Divider(),
-                pw.Table(children: [
-                  pw.TableRow(children: [
-                    pw.Text("Ranged Weapons"),
-                    pw.Text("Range"),
-                    pw.Text("Modifiers"),
-                    pw.Text("Keywords"),
-                    pw.Text("Cost"),
-                  ]),
-                  ...warrior
-                      .currentWeapon(armory)
-                      .where((w) => w.def.canRanged)
-                      .map((weapon) {
-                    return pw.TableRow(children: [
-                      pw.Text(weapon.name),
-                      pw.Text("${weapon.def.range}"),
-                      pw.Text(weapon.def.modifiers?.join("\n") ?? ""),
-                      pw.Text(weapon.def.keywords?.join("\n") ?? ""),
-                      pw.Text("${weapon.use.cost}"),
-                    ]);
-                  }),
-                ])
+                pw.Table(
+                  children: [
+                    pw.TableRow(children: [
+                      pw.Text("Ranged Weapons"),
+                      pw.Text("Range"),
+                      pw.Text("Modifiers"),
+                      pw.Text("Keywords"),
+                      pw.Text("Cost"),
+                    ]),
+                    ...warrior
+                        .currentWeapon(armory)
+                        .where((w) => w.def.canRanged)
+                        .map((weapon) {
+                      return pw.TableRow(children: [
+                        pw.Text(weapon.name),
+                        pw.Text("${weapon.def.range}"),
+                        pw.Text(weapon.def.modifiers?.join("\n") ?? ""),
+                        pw.Text(weapon.def.keywords?.join("\n") ?? ""),
+                        pw.Text("${weapon.use.cost}"),
+                      ]);
+                    }),
+                  ],
+                )
               ]
             : []),
 
         ...(melee.isNotEmpty
             ? [
                 pw.Divider(),
-                pw.Table(children: [
-                  pw.TableRow(children: [
-                    pw.Text("Melee Weapons"),
-                    pw.Text("Hands"),
-                    pw.Text("Modifiers"),
-                    pw.Text("Keywords"),
-                    pw.Text("Cost"),
-                  ]),
-                  ...warrior.meleeWeaponsOrUnarmed(armory).map((weapon) {
-                    return pw.TableRow(children: [
-                      pw.Text(weapon.name),
-                      pw.Text("${weapon.def.hands}"),
-                      pw.Text(weapon.def.modifiers?.join("\n") ?? ""),
-                      pw.Text(weapon.def.keywords?.join("\n") ?? ""),
-                      pw.Text("${weapon.use.cost}"),
-                    ]);
-                  }),
-                ])
+                pw.Table(
+                  children: [
+                    pw.TableRow(children: [
+                      pw.Text("Melee Weapons"),
+                      pw.Text("Hands"),
+                      pw.Text("Modifiers"),
+                      pw.Text("Keywords"),
+                      pw.Text("Cost"),
+                    ]),
+                    ...warrior.meleeWeaponsOrUnarmed(armory).map((weapon) {
+                      return pw.TableRow(children: [
+                        pw.Text(weapon.name),
+                        pw.Text("${weapon.def.hands}"),
+                        pw.Text(weapon.def.modifiers?.join("\n") ?? ""),
+                        pw.Text(weapon.def.keywords?.join("\n") ?? ""),
+                        pw.Text("${weapon.use.cost}"),
+                      ]);
+                    }),
+                  ],
+                )
               ]
             : []), // MELEE
 
         ...(armour.isNotEmpty
             ? [
                 pw.Divider(),
-                pw.Table(children: [
-                  pw.TableRow(children: [
-                    pw.Text("Armour"),
-                    pw.Text("Type"),
-                    pw.Text("Value"),
-                    pw.Text("Cost"),
-                  ]),
-                  ...warrior.currentArmour(armory).map((armour) {
-                    return pw.TableRow(children: [
-                      pw.Text(armour.name),
-                      pw.Text(armour.def.isBodyArmour
-                          ? "Body armour"
-                          : armour.def.isShield
-                              ? "Shield"
-                              : "-"),
-                      pw.Text("${armour.def.value ?? ""}"),
-                      pw.Text("${armour.use.cost}"),
-                    ]);
-                  }),
-                ])
+                pw.Table(
+                  children: [
+                    pw.TableRow(children: [
+                      pw.Text("Armour"),
+                      pw.Text("Type"),
+                      pw.Text("Value"),
+                      pw.Text("Cost"),
+                    ]),
+                    ...warrior.currentArmour(armory).map((armour) {
+                      return pw.TableRow(children: [
+                        pw.Text(armour.name),
+                        pw.Text(armour.def.isBodyArmour
+                            ? "Body armour"
+                            : armour.def.isShield
+                                ? "Shield"
+                                : "-"),
+                        pw.Text("${armour.def.value ?? ""}"),
+                        pw.Text("${armour.use.cost}"),
+                      ]);
+                    }),
+                  ],
+                )
               ]
             : []), // armour
 
         ...(equipment.isNotEmpty
             ? [
                 pw.Divider(),
-                pw.Table(children: [
-                  pw.TableRow(children: [
-                    pw.Text("Equipment"),
-                    pw.Text("Cost"),
-                  ]),
-                  ...warrior.currentEquipment(armory).map((equipment) {
-                    return pw.TableRow(children: [
-                      pw.Text(equipment.name),
-                      pw.Text("${equipment.use.cost}"),
-                    ]);
-                  }),
-                ]),
+                pw.Table(
+                  children: [
+                    pw.TableRow(children: [
+                      pw.Text("Equipment"),
+                      pw.Text("Cost"),
+                    ]),
+                    ...warrior.currentEquipment(armory).map((equipment) {
+                      return pw.TableRow(
+                        children: [
+                          pw.Text(equipment.name),
+                          pw.Text("${equipment.use.cost}"),
+                        ],
+                      );
+                    }),
+                  ],
+                ),
               ]
             : []), // equipment
       ],
@@ -208,35 +223,20 @@ pw.Container warriorBlock(WarriorModel warrior, Armory armory) {
 
 void printWarband(BuildContext context, Roster roster, Armory armory) async {
   final warband = context.read<WarbandModel>();
-  await Printing.layoutPdf(
-      onLayout: (PdfPageFormat format) async =>
-          renderPdf(warband, roster, armory).save());
-}
-
-class PreviewScreen extends StatelessWidget {
-  const PreviewScreen({super.key, required this.doc});
-  final pw.Document doc;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("preview"),
-      ),
-      body: PdfPreview(
-        build: (fmt) => doc.save(),
-      ),
-    );
+  if (kIsWeb) {
+    final doc = await renderPdf(warband, roster, armory);
+    downloadFile(await doc.save(), "${warband.name}.pdf");
   }
 }
 
-void previewWarband(BuildContext context, Roster roster, Armory armory) {
-  final warband = context.read<WarbandModel>();
+void downloadFile(Uint8List bytes, String filename) {
+  final web.HTMLAnchorElement anchor =
+      web.document.createElement('a') as web.HTMLAnchorElement
+        ..href = "data:application/octet-stream;base64,${base64Encode(bytes)}"
+        ..style.display = 'none'
+        ..download = filename;
 
-  Navigator.push(
-    context,
-    MaterialPageRoute(
-        builder: (ctx) =>
-            PreviewScreen(doc: renderPdf(warband, roster, armory))),
-  );
+  web.document.body!.appendChild(anchor);
+  anchor.click();
+  web.document.body!.removeChild(anchor);
 }
