dc765d0f43879bf700bcf5ad1b00fe78010518bc
Luis F. Ayuso <luis.f.ayuso@gmail.com>
2024-06-17 22:59:39 +0200

    lints

--- lib/print.dart
+++ lib/print.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:printing/printing.dart';
-import 'package:provider/provider.dart';
 import 'package:tc_thing/model/model.dart';
-import 'package:tc_thing/model/warband.dart';
-
-import 'package:tc_thing/pdf.dart';
 
 void printWarband(BuildContext context, Roster roster, Armory armory) async {
   debugPrint("stub");
